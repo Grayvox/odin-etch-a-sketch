4f2c108a73783c570083a58f815cea2821b460f8
Grayvox <144566632+Grayvox@users.noreply.github.com>
2023-12-04 16:14:21 -0500

    Add color changing effect
    Color of each square will now change with each hover detection

--- main.js
+++ main.js
@@ -5,3 +5,18 @@ for (let i = 1; i <= 256; i++) {
     containerDiv.classList.add('grid-box');
     container.appendChild(containerDiv);
 }
+
+let elements = document.querySelectorAll('.grid-box');
+
+let mouseOverEvent = (e) => {
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    e.target.style.cssText = `background-color: #${randomColor}`;
+}
+
+elements.forEach((item) => {
+    item.addEventListener('mouseover', mouseOverEvent)
+});
+
+
+
+
