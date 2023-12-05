c482b858b893cb9a0c7a9e20bc4365a2e11f7b8e
Grayvox <144566632+Grayvox@users.noreply.github.com>
2023-12-05 17:18:49 -0500

    Add game page and styling
    Adds a game page with styling to incase the entire "game"

--- main.js
+++ main.js
@@ -9,11 +9,11 @@ for (let i = 1; i <= 256; i++) {
 let elements = document.querySelectorAll('.grid-box');
 
 let mouseOverEvent = (e) => {
-    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
     e.target.style.cssText = `background-color: #${randomColor}`;
 }
 
-elements.forEach((item) => {
+elements.forEach((item) => { 
     item.addEventListener('mouseover', mouseOverEvent)
 });
 
