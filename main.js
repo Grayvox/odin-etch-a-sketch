9ea3967ed54f76ae3967106ec52ec71237722d90
Grayvox <144566632+Grayvox@users.noreply.github.com>
2023-12-18 13:46:31 -0500

    Add sketching grid generator
    Generates a grid for sketcing on page load (will change that part later)

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 const container = document.querySelector('#container');
 const adjustSizeBtn = document.querySelector('#adjust-size-button');
 
+function generateGrid(rowSize, colSize) {
+    let grid = [];
+    for (let i = 0; i < rowSize; i++) {
+        grid[i] = document.createElement('div');
+        grid[i].setAttribute('class', 'grid-row');
+
+        let boxes = [];
+        for (let j = 0; j < colSize; j++) {
+            boxes[j] = document.createElement('div');
+            boxes[j].setAttribute('class', 'grid-box');
+        }
+
+        grid[i].append(...boxes);
+
+    }
+    container.append(...grid);
+}
+
+generateGrid(16, 16);
+
 
 
